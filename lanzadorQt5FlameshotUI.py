8aad5d49cb20ec1253c5a7828f51ef1745aba1a7
Arg0s <ivan.rincon76@gmail.com>
2017-11-03 21:46:30 +0100

    Pequeña corrección

--- lanzadorQt5FlameshotUI.py
+++ lanzadorQt5FlameshotUI.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '/media/ivan/Acer/Users/ivanr/Proyectos/Python/PyCharmProjects/LanzadorQt5-Flameshot/lanzadorQt5Flameshot.ui'
 #
 # Created by: PyQt5 UI code generator 5.7
 #
